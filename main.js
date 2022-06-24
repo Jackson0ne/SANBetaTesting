47b174587e2c10047bbe0143c85c891284c28782
o Jackson 1 o <73694667+Jackson0ne@users.noreply.github.com>
2022-06-24 15:20:54 +0100

    Update Beta Files
    - Updated translations
    - Added "Game Art" option in Background Style dropdowns

--- main.js
+++ main.js
@@ -225,9 +225,11 @@ const startapp = () => {
         });
 
         var gameicon;
+        var gameartimg
 
-        ipcMain.on('gameicon', (event, gameiconsrc) => {
+        ipcMain.on('gameicon', (event, gameiconsrc, gameartimgsrc) => {
             gameicon = gameiconsrc;
+            gameartimg = gameartimgsrc
         });
 
         ipcMain.on('notifywin', function(event, queueobj) {
@@ -634,7 +636,7 @@ const startapp = () => {
                 notifywin.setSize(width, height)
 
                 notifywin.show();
-                notifywin.webContents.send('notifymain', queueobj.achievement, queueobj.title, queueobj.desc, queueobj.icon, queueobj.screenshot, queueobj.percent, queueobj.audio, gameicon);
+                notifywin.webContents.send('notifymain', queueobj.achievement, queueobj.title, queueobj.desc, queueobj.icon, queueobj.screenshot, queueobj.percent, queueobj.audio, gameicon, gameartimg);
                 // notifywin.webContents.openDevTools({ mode: 'detach' })
             });
 
